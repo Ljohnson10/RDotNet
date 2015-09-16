730a8e41621180482150112fe69dbbdfc67f89c5
Lucas Johnson <lucas-m-johnson@uiowa.edu>
2015-09-16 16:29:30 -0500

    first commit

---
+++ RdotTest/RdotTest/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace RdotTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // readBtn
+            // 
+            this.readBtn.Location = new System.Drawing.Point(23, 13);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.readBtn.Size = new System.Drawing.Size(75, 23);
+            this.readBtn.TabIndex = 0;
+            this.readBtn.Text = "Read CSV";
+            this.readBtn.UseVisualStyleBackColor = true;
+            this.readBtn.Click += new System.EventHandler(this.readBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(484, 443);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(575, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(575, 235);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 534);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.readBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ RdotTest/RdotTest/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RDotNet;
+
+namespace RdotTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+             
+            string dlldir = @"C:\Program Files\R\R-3.2.1\bin\i386";
+            REngine.SetEnvironmentVariables(dlldir);
+           
+
+        }
+      
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void readBtn_Click(object sender, EventArgs e)
+        {
+           REngine engine = REngine.GetInstance();
+ 
+            try
+            {
+                // import csv file
+                DataFrame dataset = engine.Evaluate("dataset<-read.table(file.choose(), header=TRUE, sep = ',')").AsDataFrame();
+ 
+                // retrieve the data frame
+                
+
+                for (int i = 0; i < dataset.ColumnCount; ++i)
+                {
+                    dataGridView1.ColumnCount++;
+                    dataGridView1.Columns[i].Name = dataset.ColumnNames[i];
+                }
+
+                for (int i = 0; i < dataset.RowCount; ++i)
+                {
+                    dataGridView1.RowCount++;
+                    dataGridView1.Rows[i].HeaderCell.Value = dataset.RowNames[i];
+
+                    for (int k = 0; k < dataset.ColumnCount; ++k)
+                    {
+                        dataGridView1[k, i].Value = dataset[i, k];
+
+                    }
+
+                }
+                
+
+              
+            }
+ 
+            catch
+            {
+                MessageBox.Show(@"Equation error.");
+            }
+
+            
+        }
+        }
+    
+}
